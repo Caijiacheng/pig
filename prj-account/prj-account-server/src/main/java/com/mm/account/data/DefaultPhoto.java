349dd2f16c29fff4ddf509a6ec600734a7d93ae6
caijiacheng <caihua2006@gmail.com>
2014-09-09 10:50:27 +0800

    add TestDefaultPhoto

--- prj-account/prj-account-server/src/main/java/com/mm/account/data/DefaultPhoto.java
+++ prj-account/prj-account-server/src/main/java/com/mm/account/data/DefaultPhoto.java
@@ -1,18 +1,23 @@
 package com.mm.account.data;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
+import java.io.OutputStream;
 
-import org.restexpress.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import redis.clients.jedis.Jedis;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
-import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSink;
 import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
 import com.mm.account.db.RedisDB;
 import com.mm.account.error.NotExistException;
+import com.mm.account.error.UnknowAccException;
 
 
 /**
@@ -26,11 +31,19 @@ import com.mm.account.error.NotExistException;
 
 public class DefaultPhoto implements IPhoto {
 	
+	
+	
+	
 	ByteSource in;
 
 	long userid;
 	
-	public DefaultPhoto() {
+	DefaultPhoto(String file) {
+		in = Files.asByteSource(new File(file));
+	}
+	
+	DefaultPhoto(File f) {
+		in = Files.asByteSource(f);
 	}
 	
 	@Override
@@ -47,44 +60,107 @@ public class DefaultPhoto implements IPhoto {
 	static class Url implements IUrl
 	{
 
+		Url(byte[] key)
+		{
+			reskey = key;
+		}
+		byte[] reskey;
 		@Override
 		public String url() {
-			return null;
+			return new String(reskey, Charsets.UTF_8);
 		}
 	}
 	
 	static public class Service implements IPhotoService
 	{
-
+		
+		
+		static Logger LOG = LoggerFactory.getLogger(Service.class);
+		
+		static String downloadDir = "./downlaod/";
+		public Service() {
+			File baseDir = new File(downloadDir);
+			if (!baseDir.exists())
+			{
+				baseDir.mkdirs();
+			}
+			
+		}
+		
 		String getIncrPhotoKey(long userid)
 		{
 			return String.format("DefaultPhoto.Service:%s:PhotoCnt", userid);
 		}
 		
-		String getPhotoIDKey(long userid, long photoid)
+		byte[] getPhotoIDKey(long userid, long photoid)
 		{
-			return String.format("DefaultPhoto.Service:%s:Photo:%s", userid, photoid);
+			return String.format("DefaultPhoto.Service:%s:Photo:%s", userid, photoid).getBytes(Charsets.UTF_8);
 		}
 		
 		@Override
 		public IUrl upload(IPhoto photo) {
 			
 			DefaultPhoto ph = (DefaultPhoto)photo;
+		
 			
 			RedisDB db = new RedisDB();
 			try(Jedis handle = db.getConn())
 			{
 				Long photoid = handle.incr(getIncrPhotoKey(ph.userid));
 				
+				byte[] b = new byte[16384];
+				InputStream in = ph.data();
+				
+				byte[] photokey = getPhotoIDKey(ph.userid, photoid);
+				while(true){
+					int num = in.read(b);
+					if (num < 0 )
+					{
+						break;
+					}
+					byte[] bb = new byte[num];
+					System.arraycopy(b, 0, bb, 0, num);
+					LOG.debug("append .. {}", num);
+					handle.append(photokey, bb);
+				}
+				return new Url(photokey);
+			} catch (IOException e) {
+				throw new UnknowAccException(e);
 			}
-			
-			return null;
 		}
 
+		
+		
 		@Override
 		public IPhoto download(IUrl url) {
+			Url u = (Url)url;
 			
-			return null;
+			File f = new File(downloadDir + u.url());
+			ByteSink sink = Files.asByteSink(f);
+			
+			RedisDB db = new RedisDB();
+			int bytes = 16384;
+			try(Jedis handle = db.getConn())
+			{
+				try(OutputStream ots = sink.openBufferedStream())
+				{
+					int begin = 0;
+					while(true)
+					{
+						byte[] bb = handle.getrange(u.reskey, begin, begin + bytes);
+						ots.write(bb);
+						begin += bb.length;
+						LOG.debug("getrange ..{}", bb.length);
+						if (bb.length < bytes)
+						{
+							return new DefaultPhoto(f);
+						}
+					}
+				}
+				
+			} catch (IOException e) {
+				throw new UnknowAccException(e);
+			}
 		}
 		
 	}
---
+++ prj-account/prj-account-server/src/test/java/com/mm/account/data/TestDefaultPhoto.java
@@ -0,0 +1,79 @@
+package com.mm.account.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDefaultPhoto {
+
+	
+	  public static String bytesToString(byte[] data) {
+          char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
+                          'e', 'f'};
+          char[] temp = new char[data.length * 2];
+          for (int i = 0; i < data.length; i++) {
+                  byte b = data[i];
+                  temp[i * 2] = hexDigits[b >>> 4 & 0x0f];
+                  temp[i * 2 + 1] = hexDigits[b & 0x0f];
+          }
+          return new String(temp);
+
+	  }
+
+	
+	public String getMd5(InputStream fis) throws NoSuchAlgorithmException, IOException
+	{
+		   MessageDigest md = MessageDigest.getInstance("MD5");
+           byte[] buffer = new byte[8192];
+           int length = -1;
+           while ((length = fis.read(buffer)) != -1) {
+                   md.update(buffer, 0, length);
+           }
+           return bytesToString(md.digest());
+	}
+	
+	String test_file_name = "src/test/resources/aio1j.html";
+	
+	@Test
+	public void testAssertFileIsExist()
+	{
+		String fname = test_file_name;
+		File f = new File(fname);
+		
+		Assert.assertTrue(f.exists());
+	}
+	
+	@Test
+	public void testDefaultPhoto() throws IOException, NoSuchAlgorithmException
+	{
+		IPhotoService service = new DefaultPhoto.Service();
+		
+		
+		DefaultPhoto raw_photo = new DefaultPhoto(test_file_name);
+		
+		IUrl url  = service.upload(raw_photo);
+		
+		IPhoto db_photo = service.download(url);
+
+		String raw_md5;
+		try(InputStream fis = raw_photo.data())
+		{
+			raw_md5 = getMd5(fis);
+		}
+		
+		String db_md5;
+		try(InputStream fis = db_photo.data())
+		{
+			db_md5 = getMd5(fis);
+		}
+		
+		Assert.assertEquals(raw_md5, db_md5);
+		
+	}
+
+}
