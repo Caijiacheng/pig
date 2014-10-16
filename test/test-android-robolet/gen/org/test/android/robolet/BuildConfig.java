9429aac12d1811445b927da509d1a48f628a1030
caijiacheng <caijiacheng@caijiachengdeMacBook-Pro.local>
2014-10-16 10:57:29 +0800

    add android prj example

---
+++ test/test-android-robolet/gen/org/test/android/robolet/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.test.android.robolet;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ test/test-android-robolet/src/main/java/org/test/android/robolet/HelloAndroidActivity.java
@@ -0,0 +1,14 @@
+package org.test.android.robolet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
